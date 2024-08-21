e2e040e5682dd67c6d85d23499ffbaf995fee0b1
AdrianKrampikowski <info@adrian-krampikowski.com>
2024-08-21 13:03:36 +0200

    add middleware to update Userstatus

--- cafemangementsystem_backend/apis/users/index.js
+++ cafemangementsystem_backend/apis/users/index.js
@@ -7,8 +7,7 @@ var checkrole = require("../../middlewares/checkrole")
 router.route("/signup").post(UserController.signUp);
 router.route("/login").post(UserController.login);
 router.route("/getAllUsers").get(auth, UserController.getAllUsers);
-router.route("/updateUser").patch(UserController.updateUser);
-// router.route("/updateUser").patch(auth, checkrole, UserController.updateUser);
+router.route("/updateUser").patch(auth, checkrole, UserController.updateUser);
 router.route("/changepassword").patch(auth, checkrole, UserController.changePassword);
 router.route("/changeOwnPassword").put(UserController.changeOwnPassword),
 
